8c5d121a29b3cd83293bd674d1be675a09a2b25f
Alexey Melnik <amelnik75@yandex.ru>
2025-06-26 22:31:38 +0300

    format test_program and new requirements

--- test_program.py
+++ test_program.py
@@ -5,9 +5,11 @@ except ImportError:
 
 EXPECTED_FUNC_NAME = 'say_hello'
 
+
 def test_say_hello_exists():
     assert hasattr(practicum, EXPECTED_FUNC_NAME), (
         f'Функция `{EXPECTED_FUNC_NAME}` не обнаружена в модуле `practicum`')
+
 
 def test_say_hello_run_without_exceptions():
     try:
@@ -16,4 +18,4 @@ def test_say_hello_run_without_exceptions():
         raise AssertionError(
             f'При запуске функции `{EXPECTED_FUNC_NAME}` возникло '
             f'исключение: {type(error).__name__}: {error}`'
-        ) 
+        )
